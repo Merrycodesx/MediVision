3f0ec84eab3c931c259ca48d6737cda7a189b31b
mihret1221 <mihretlealem839@gmail.com>
2026-03-31 12:46:19 -0900

    change

--- frontend/components/UserManagement.js
+++
@@ -1,9 +0,0 @@
-export default function UserManagement({ setCurrentFeature }) {
-  return (
-    <section>
-      <h2>User Management</h2>
-      <p>Use the server API for user management.</p>
-      <button onClick={() => setCurrentFeature('patients')}>Back</button>
-    </section>
-  );
-}
